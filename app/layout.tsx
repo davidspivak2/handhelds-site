03b37b54ab9632468979fe5e69ba338736330968
davidspivak2 <davidspivak2@gmail.com>
2025-05-04 19:49:59 +0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ fontFamily: 'sans-serif', background: '#f9f9f9', margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
